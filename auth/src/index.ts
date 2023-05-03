04d26ca409588bded350f46eb9c97c2f2cf99dcf
zaoui amine <aminedazaoui@gmail.com>
2023-05-03 09:03:38 +0100

    completion of auth service

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -1,9 +1,13 @@
 import express from "express";
+import "express-async-errors";
+import mongoose from "mongoose";
+
 import { currentUserRouter } from "./routes/current-user";
 import { signinRouter } from "./routes/signin";
 import { signoutRouter } from "./routes/signout";
 import { signupRouter } from "./routes/signup";
 import { errorHandler } from "./middlewares/error-handler";
+import { NotFoundError } from "./errors/not-found-error";
 
 const app = express();
 
@@ -13,8 +17,23 @@ app.use(currentUserRouter);
 app.use(signinRouter);
 app.use(signoutRouter);
 app.use(signupRouter);
+
+app.all("*", async (req, res) => {
+  throw new NotFoundError();
+});
+
 app.use(errorHandler);
 
-app.listen(3000, () => {
-  console.log("listening on port 3000!!!!");
-});
+const start = async () => {
+  try {
+    mongoose.connect("mongodb://auth-mongo-srv:27017/auth");
+    console.log("connected to db");
+  } catch (error) {
+    console.error(error);
+  }
+  app.listen(3000, () => {
+    console.log("listening on port 3000!!");
+  });
+};
+
+start();
--- auth/src/middlewares/error-handler.ts
+++ auth/src/middlewares/error-handler.ts
@@ -1,4 +1,8 @@
 import { Request, Response, NextFunction } from "express";
+import { RequestValidationError } from "../errors/request-validation-error";
+import { DatabaseConnectionError } from "../errors/database-connection-error";
+import { NotFoundError } from "../errors/not-found-error";
+import { BadRequestError } from "../errors/bad-request-error";
 
 export const errorHandler = (
   err: Error,
@@ -6,9 +10,18 @@ export const errorHandler = (
   res: Response,
   next: NextFunction
 ) => {
-  console.log("something went wrong");
+  if (err instanceof RequestValidationError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  }
+  if (err instanceof DatabaseConnectionError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  }
+  if (err instanceof NotFoundError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  }
+  if (err instanceof BadRequestError) {
+    return res.status(err.statusCode).send({ errors: err.serializeErrors() });
+  }
 
-  res.status(400).send({
-    message: err.message,
-  });
+  res.status(500).send({ errors: { message: "something went wrong" } });
 };
