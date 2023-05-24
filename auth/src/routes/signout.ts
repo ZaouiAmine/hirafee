1584a361fc0ab6d6eab9dd62e7580edea98a85e5
zaoui amine <aminedazaoui@gmail.com>
2023-05-24 11:13:58 +0100

    added the npm package

--- auth/src/routes/signout.ts
+++ auth/src/routes/signout.ts
@@ -3,11 +3,8 @@ import express from "express";
 const router = express.Router();
 
 router.post("/api/users/signout", (req, res) => {
-<<<<<<< HEAD
   console.log("signout route");
 
-=======
->>>>>>> origin/malak
   req.session = null;
   res.send("signed out");
 });
