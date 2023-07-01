ae5ebd74ac702853a543f7aecd968a8c2050e8f9
unknown <boudamalak@gmail.com>
2023-07-01 16:36:14 +0200

    add footer

--- client/components/homePages/NotSignedPage.jsx
+++ client/components/homePages/NotSignedPage.jsx
@@ -2,13 +2,17 @@ import Link from "next/link";
 import Image from "next/image";
 import hero from "@/public/heroBg.png";
 import dots from "@/public/landingpage/dots.png";
-
 import pics from "@/public/landingpage/steps/pics.png";
-
 import gig1 from "@/public/landingpage/steps/gig1.png";
 import gig2 from "@/public/landingpage/steps/gig2.png";
 import gig3 from "@/public/landingpage/steps/gig3.png";
 import gig4 from "@/public/landingpage/steps/gig4.png";
+
+import facebook from "@/public/icons/facebook.png";
+import twitter from "@/public/icons/twitter.png";
+import instagram from "@/public/icons/instagram.png";
+import linkedin from "@/public/icons/linkedin.png";
+
 import applygig from "@/public/landingpage/steps/applygig.png";
 import handshake from "@/public/landingpage/steps/handshake.png";
 
@@ -118,9 +122,44 @@ const NotSignedPage = () => {
           <Image alt="" src={handshake} width={"500"} className=" " />
         </div>
       </section>
-      <section className="container flex p-4 py-16 h-128 bg-accent">
-        categories
-      </section>
+
+        <footer className="bg-accent w-full py-4 px-8 text-dark text-center">
+        <div className="container mx-auto">
+          <ul className="flex justify-center gap-6">
+            <li>
+              <Link href="/">Home</Link>
+            </li>
+            <li>
+              <Link href="/about">About Us</Link>
+            </li>
+            <li>
+              <Link href="/contact">Contact</Link>
+            </li>
+            <li>
+              <Link href="/services">Services</Link>
+            </li>
+            <li>
+              <Link href="/policy"> Privacy Policy</Link>
+            </li>
+          </ul>
+          <div className="mt-4">
+          <ul className="flex justify-center gap-6">
+            <li>
+            <Image src={facebook} alt="Custom Icon" width={24} height={24} />
+            </li>
+            <li>
+            <Image src={instagram} alt="Custom Icon" width={24} height={24} />
+            </li>
+            <li>
+            <Image src={twitter} alt="Custom Icon" width={24} height={24} />
+            </li>
+            <li>
+            <Image src={linkedin} alt="Custom Icon" width={24} height={24} />
+            </li>
+          </ul>
+          </div>
+        </div>
+      </footer>
     </main>
   );
 };
